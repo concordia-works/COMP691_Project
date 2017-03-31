0ed6104962be79f481d214f1241c5ca8c5f438ee
midnightblur <minhvq27@gmail.com>
2017-03-31 16:08:59 -0400

    Remove Mahout display logging to console

--- src/main/java/com/comp691/Project/Main.java
+++ src/main/java/com/comp691/Project/Main.java
@@ -3,10 +3,15 @@ package com.comp691.Project;
 import com.comp691.CollaborativeFiltering.*;
 
 public class Main {
-
+	// Stop Apache Mahout from displaying logging to console
+	static {
+	      System.setProperty("org.apache.commons.logging.Log",
+	                         "org.apache.commons.logging.impl.NoOpLog");
+	   }
+	
 	public static void main(String[] args) {
 		UserBasedCF.execute();
-		ItemBasedCF.execute();
+		//ItemBasedCF.execute();
 	}
 
 }
